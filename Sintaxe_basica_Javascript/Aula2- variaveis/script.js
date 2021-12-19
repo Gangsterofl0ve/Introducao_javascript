81116758c1ffc105048058968f3eb6c38113d018
Gangsterofl0ve <gangsteroflove93@gmail.com>
2021-12-19 12:16:28 -0300

    Dando sequencia as aulas

---
+++ Sintaxe_basica_Javascript/Aula2- variaveis/script.js
@@ -0,0 +1,106 @@
+//tipos primitivos
+
+//boolean
+/*
+var VouF = false;
+console.log(typeof(VouF));
+
+//number
+var numeroQualquer= 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'Denner';
+console.log(typeof(nome)); */
+
+//como declarar
+/* var variavel ='Denner';
+variavel = 'Cruz';
+console.log(variavel);*/
+
+/* let variavel2 = 'Denner';
+variavel2 = 'cruz;'
+console.log(variavel2);*/
+
+/* const constante = 'Denner';
+constante = 'Cruz';
+console.log(constante);*/
+
+/* var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+  let escopoLocalInterno = 'local'; 
+  console.log(escopoLocalInterno);
+}
+escopoLocal(); */
+
+/*
+//atribuição
+var atribucao = 'Denner';
+
+//comparação
+var comparacao = 0 == 0;
+console.log(comparacao);
+
+// comparação identica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);*/
+
+/*
+//adição
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtração
+var sub = 1 - 1;
+console.log(sub);
+
+//multiplicação
+var multi = 2*3;
+console.log(multi);
+
+//divisão
+var divReal = 6 / 2;
+console.log(divReal);
+
+var divInteira = 5 % 2;
+console.log(divInteira);
+
+//potenciação
+var pot = 2 ** 10;
+console.log(pot);
+
+//maior, menor ou igual
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual); */
+
+/* var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+var nao = !true;
+console.log(nao);
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
